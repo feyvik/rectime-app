146a2d91060ac61172b18efdeb9a9761ad93fe10
feyvik <vivianemma97@gmail.com>
2022-12-27 21:02:50 +0100

    Set up project boiler plate

---
+++ src/app/registration/registration.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.page.html',
+  styleUrls: ['./registration.page.scss'],
+})
+export class RegistrationPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+  slideOpts = {
+    // initialSlide: 1,
+    // speed: 400,
+  };
+}
